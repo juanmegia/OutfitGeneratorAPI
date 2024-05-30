3715c9a3e350f769f56b67ea9dbcdf4172afbf46
juanmegia <juanmegia01@gmail.com>
2024-05-30 19:19:01 +0200

    8

--- OutfitGeneratorAPI/models.py
+++ OutfitGeneratorAPI/models.py
@@ -47,7 +47,7 @@ class Piece(models.Model):
 
 class Outfit(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     pieces = models.ManyToManyField(Piece)
 
